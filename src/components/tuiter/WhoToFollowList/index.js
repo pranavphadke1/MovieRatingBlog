8c7713dccbb65a3769a867dead703bc2c078afac
pranavphadke1 <pranavmphadke@gmail.com>
2022-04-26 00:18:31 -0400

    Made users dynamic with Mongo and Node Server

--- src/components/tuiter/WhoToFollowList/index.js
+++ src/components/tuiter/WhoToFollowList/index.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, {useEffect} from "react";
+import {useSelector,useDispatch} from "react-redux";
 import WhoToFollowListItem from "./WhoToFollowListItem";
-import {useSelector} from "react-redux";
+import{findAllUsers} from "../../../actions/who-actions";
+
 const WhoToFollowList = () => {
     const who = useSelector((state) => state.who);
+    const dispatch = useDispatch();
+    useEffect(() =>
+                  findAllUsers(dispatch),
+              []);
     return (
         <ul className="list-group wd-list-group-override">
             <li className="list-group-item">
